e893649ab301e50b43e6118ea9cdbfc84309979d
Sandip Thapa <sandip.thapa543@gmail.com>
2020-01-09 23:24:56 +0545

    initialy

---
+++ app/src/main/java/com/example/taskmanager/activities/DashboardActivity.java
@@ -0,0 +1,76 @@
+package com.example.taskmanager.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.taskmanager.R;
+import com.example.taskmanager.api.UsersAPI;
+import com.example.taskmanager.model.User;
+import com.example.taskmanager.strictmode.StrictModeClass;
+import com.example.taskmanager.url.Url;
+import com.squareup.picasso.Picasso;
+
+import java.io.InputStream;
+import java.net.URL;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    ImageView imgProgileImg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        imgProgileImg = findViewById(R.id.imgProgileImg);
+
+        findViewById(R.id.btnAdd).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DashboardActivity.this, AddTaskActivity.class));
+            }
+        });
+
+        loadCurrentUser();
+    }
+
+    private void loadCurrentUser() {
+
+        UsersAPI usersAPI = Url.getInstance().create(UsersAPI.class);
+        Call<User> userCall = usersAPI.getUserDetails(Url.token);
+
+        userCall.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+
+                String imgPath = Url.imagePath +  response.body().getImage();
+
+                Picasso.get().load(imgPath).into(imgProgileImg);
+
+
+//                StrictModeClass.StrictMode();
+//                try {
+//                    URL url = new URL(imgPath);
+//                    imgProgileImg.setImageBitmap(BitmapFactory.decodeStream((InputStream) url.getContent()));
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+
+                Toast.makeText(DashboardActivity.this, "Error " + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/taskmanager/activities/LoginActivity.java
@@ -0,0 +1,68 @@
+package com.example.taskmanager.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.taskmanager.R;
+import com.example.taskmanager.bll.LoginBLL;
+import com.example.taskmanager.strictmode.StrictModeClass;
+import com.example.taskmanager.url.Url;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private EditText etUsername, etPassword;
+    private TextView tvSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        tvSignup = findViewById(R.id.tvSignup);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        etUsername.setText("");
+        etPassword.setText("");
+
+        tvSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(); }
+        });
+    }
+
+    private void login() {
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+
+        LoginBLL loginBLL = new LoginBLL();
+
+        StrictModeClass.StrictMode();
+        if (loginBLL.checkUser(username, password)) {
+            Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(this, "Either username or password is incorrect", Toast.LENGTH_SHORT).show();
+            etUsername.requestFocus();
+        }
+    }
+}
